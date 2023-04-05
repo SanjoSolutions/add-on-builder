0d48b937cad4ae1822fe0ad30b233cbf84fc517a
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-05 11:13:02 +0200

    Updates for building

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ import * as process from 'node:process'
 import { fileExists } from './unnamed/file/fileExists.js'
 
 export async function build() {
-  const buildDirectory = 'build'
+  const addOnName = path.basename(process.cwd())
+  const buildDirectory = '../../build/' + addOnName + '/'
   await fs.rm(buildDirectory, { recursive: true, force: true })
   await fs.mkdir(buildDirectory, { recursive: true })
 
-  const addOnName = path.basename(process.cwd())
-  const addOnTocFilePath = `./${ addOnName }/${ addOnName }.toc`
+  const addOnTocFilePath = `./${ addOnName }.toc`
 
   const dependenciesToCopy = await retrieveDependencies(addOnTocFilePath)
   const dependenciesToCopySet = new Set(dependenciesToCopy)
   for (let index = 0; index < dependenciesToCopy.length; index++) {
     const dependency = dependenciesToCopy[index]
-    const dependencyPath = `AddOns/${ dependency }`
-    await fs.cp(dependencyPath, path.join(buildDirectory, dependency), { recursive: true })
+    const dependencyPath = `../${ dependency }`
+    await copyAddOn(dependencyPath, path.join(buildDirectory, dependency))
 
     const dependencies2 = await retrieveDependencies(`${ dependencyPath }/${ dependency }.toc`)
     for (const dependency2 of dependencies2) {
@@ -29,9 +29,7 @@ export async function build() {
     }
   }
 
-  await fs.cp(addOnName, `${ buildDirectory }/${ addOnName }`, { recursive: true })
-  await fs.cp('LICENSE', `${ buildDirectory }/${ addOnName }/LICENSE`)
-  await fs.cp('README.md', `${ buildDirectory }/${ addOnName }/README.md`)
+  await copyAddOn('.', `${ buildDirectory }/${ addOnName }`)
 
   async function generateOutputFileName() {
     const version = await retrieveVersion(addOnTocFilePath)
@@ -45,11 +43,24 @@ export async function build() {
 
   const outputFileName = await generateOutputFileName()
 
-  if (await fileExists(outputFileName)) {
+  if (await fileExists(path.join(buildDirectory, '..', outputFileName))) {
     console.error(`Output file "${ outputFileName }" already exists. If you intend to overwrite it, please delete the file manually before running the build script.`)
   } else {
     child_process.execSync(`7z a -tzip ../${ outputFileName } *`, {
       cwd: buildDirectory,
     })
   }
+}
+
+const filesAndFoldersToLeaveOut = new Set([
+  '.idea', '.git', '.gitignore', '.gitmodules',
+])
+
+async function copyAddOn(addOnSourcePath, addOnDestinationPath) {
+  await fs.cp(addOnSourcePath, addOnDestinationPath, {
+    recursive: true,
+    filter(source) {
+      return !filesAndFoldersToLeaveOut.has(path.basename(source)) && !source.endsWith('.bat')
+    },
+  })
 }
