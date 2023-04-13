13cd7eec7568dc2b5c351b001fe1d232100aeeec
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-04-13 09:39:57 +0200

    Some work for automatic packaging with CurseForge

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import { exec } from '@sanjo/exec'
+import { readFile } from '@sanjo/read-file'
 import { retrieveDependencies, retrieveVersion } from '@sanjo/toc'
 import * as child_process from 'node:child_process'
 import * as fs from 'node:fs/promises'
@@ -63,4 +65,97 @@ async function copyAddOn(addOnSourcePath, addOnDestinationPath) {
       return !filesAndFoldersToLeaveOut.has(path.basename(source)) && !source.endsWith('.bat')
     },
   })
+}
+
+export async function createPackageMetaFile(addOnPath) {
+  const addOnName = path.basename(addOnPath)
+
+  let content = `package-as: ${ addOnName }
+enable-nolib-creation: no
+externals:  
+`
+
+  const dependencies = await retrieveAllDependencies(addOnPath)
+
+  for (const dependency of dependencies) {
+    const gitURL = await retrieveDependencyGitURL(dependency.dependencyPath)
+    const version = await retrieveVersion(determineTOCFilePath(addOnPath))
+    content += `  libs/${ dependency.dependency }:\n` +
+      `    url: ${ gitURL }\n` +
+      `    tag: ${ version }\n`
+  }
+
+  // TODO: Include dependencies also as required-dependencies? (for extra revenue sharing?)
+}
+
+function determineTOCFilePath(addOnPath) {
+  const addOnName = path.basename(addOnPath)
+  const addOnTocFilePath = `${ addOnPath }/${ addOnName }.toc`
+  return addOnTocFilePath
+}
+
+async function retrieveAllDependencies(addOnPath) {
+  const result = []
+
+  const addOnTocFilePath = determineTOCFilePath(addOnPath)
+
+  const dependenciesToCopy = await retrieveDependencies(addOnTocFilePath)
+  const dependenciesToCopySet = new Set(dependenciesToCopy)
+  for (let index = 0; index < dependenciesToCopy.length; index++) {
+    const dependency = dependenciesToCopy[index]
+    const dependencyPath = path.resolve(addOnPath, `../${ dependency }`)
+
+    result.push({
+      dependency,
+      dependencyPath,
+    })
+
+    const dependencies2 = await retrieveDependencies(`${ dependencyPath }/${ dependency }.toc`)
+    for (const dependency2 of dependencies2) {
+      if (!dependenciesToCopySet.has(dependency2)) {
+        dependenciesToCopySet.add(dependency2)
+        dependenciesToCopy.push(dependency2)
+      }
+    }
+  }
+
+  return result
+}
+
+async function retrieveDependencyGitURL(dependencyPath) {
+  const dotGitContent = await readFile(path.join(dependencyPath, '.git'))
+  const gitDirRegExp = /gitdir: (.+)/
+  const match = gitDirRegExp.exec(dotGitContent)
+  const relativeGitModulePath = match[1]
+  const gitModulePath = path.relative(dependencyPath, relativeGitModulePath)
+  const remotes = await exec(
+    'C:\\Program Files\\Git\\cmd\\git.exe remote -v',
+    {
+      cwd: gitModulePath,
+    },
+  )
+  const gitURL = convertSSHToHTTPS(extractOriginFetch(remotes))
+  return gitURL
+}
+
+function extractOriginFetch(remotes) {
+  const lines = remotes.split('\n')
+  const partsRegExp = /[^ ]+ +[^ ]+ +[^ ]+/
+  for (const line of lines) {
+    const match = partsRegExp.exec(line)
+    if (match[1] === 'origin' && match[3] === '(fetch)') {
+      return match[2]
+    }
+  }
+  return null
+}
+
+function convertSSHToHTTPS(url) {
+  const githubSSHURLRegExp = /^git@(github.com):(.+?\.git)$/
+  const match = githubSSHURLRegExp.exec(url)
+  if (match) {
+    return `https://${ match[1] }/${ match[2] }`
+  } else {
+    return url
+  }
 }
